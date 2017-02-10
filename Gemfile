0e18f748316a33fbf095a742af217d9cfbd4741c
Michail Markov <avis-hermetis@yandex.ru>
2017-02-10 15:08:17 +1000

    Add log_in_spec

--- Gemfile
+++ Gemfile
@@ -55,6 +55,8 @@ end
 
 group :test do
   gem 'shoulda-matchers', '~> 3.1'
+
+  gem 'capybara'
 end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ spec/acceptance/log_in_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature "User log in", %q{
+  In order to ask question
+  As a user
+  I want to log in
+} do
+  scenario "Registered user tries to log in" do
+    User.create!(email: "user@test.com", password: "12345678")
+
+    click_on new_user_session_path
+    fill_in "Email", with: "user@test.com"
+    fill_in "Password", with: "12345678"
+    click_on "Log in"
+
+    expect(page).to have_content "Logged in successfully."
+    expect(current_path).to eq root_path
+  end
+  scenario "Not registered user tries to log in" do
+    click_on new_user_session_path
+    fill_in "Email", with: "invalid@test.com"
+    fill_in "Password", with: "12345678"
+    click_on "Log in"
+
+    expect(page).to have_content "Invalid email or password."
+    expect(current_path).to eq new_user_session_path
+  end
+end
