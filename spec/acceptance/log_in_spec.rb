23fb9520bf3fd3beefad86dd2bbdf427eaf0c4ab
Michail Markov <avis-hermetis@yandex.ru>
2017-02-11 14:29:41 +1000

    Everything is working just fine

--- spec/acceptance/log_in_spec.rb
+++ spec/acceptance/log_in_spec.rb
@@ -8,22 +8,23 @@ feature "User log in", %q{
   scenario "Registered user tries to log in" do
     User.create!(email: "user@test.com", password: "12345678")
 
-    click_on new_user_session_path
+    visit new_user_session_path
     fill_in "Email", with: "user@test.com"
     fill_in "Password", with: "12345678"
     click_on "Log in"
     save_and_open_page
 
-    expect(page).to have_content "Logged in successfully."
+    expect(page).to have_content "Signed in successfully."
     expect(current_path).to eq root_path
   end
   scenario "Not registered user tries to log in" do
-    click_on new_user_session_path
+    visit new_user_session_path
     fill_in "Email", with: "invalid@test.com"
     fill_in "Password", with: "12345678"
     click_on "Log in"
 
-    expect(page).to have_content "Invalid email or password."
+    expect(page).to have_content "Invalid Email or password. Log in Email Password Remember
+    me Sign up Forgot your password?"
     expect(current_path).to eq new_user_session_path
   end
 end
