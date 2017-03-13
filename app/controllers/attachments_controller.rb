d0e26e623d2102296232919b3124cf00baab380d
Michail Markov <avis-hermetis@yandex.ru>
2017-03-13 16:18:46 +1000

    Add attachment_controller-spec

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -4,7 +4,7 @@ class AttachmentsController < ApplicationController
   def destroy
     @attachment = Attachment.find(params[:id])
     if current_user&.author_of?(@attachment.attachable)
-    @attachment.destroy
+      @attachment.destroy
     else
       head 403
     end
--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe AttachmentsController, type: :controller do
+  let!(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
+  let!(:question) { create(:question, user: user) }
+  let!(:file) {create(:attachment, attachable: question)}
 
+  describe 'DELETE #destroy' do
+
+    context 'Authenticated user is the author of attachable object' do
+      before {sign_in user}
+
+      it 'assigns requested attachment form the db to the @attachmebt variable' do
+        patch :destroy, params: {id: file, format: :js}
+        expect(assigns(:attachment)).to eq file
+      end
+      it 'deletes an attachment' do
+        expect {patch :destroy, params: {id: file, format: :js}}.to change(Attachment, :count).by(-1)
+      end
+      it 'renders destroy template' do
+        patch :destroy, params: {id: file, format: :js}
+        expect(response).to render_template :destroy
+      end
+    end
+
+    context 'Authenticated user is NOT author of attachable object' do
+      before {sign_in other_user}
+
+      it 'failes to delete an attachment' do
+        expect { patch :destroy, params: {id: file, format: :js} }.to_not change(Attachment, :count)
+      end
+      it 'renders status 403 without destroy template' do
+        patch :destroy, params: {id: file, format: :js}
+        expect(response).to_not render_template :destroy
+        expect(response).to have_http_status(403)
+      end
+    end
+
+    context 'NOT authenticated user' do
+
+      it 'failes to delete an attachment' do
+        expect {patch :destroy, params: {id: file, format: :js}}.to_not change(Attachment, :count)
+      end
+      it 'renders status 403 without destroy template' do
+        patch :destroy, params: {id: file, format: :js}
+        expect(response).to_not render_template :destroy
+        expect(response).to have_http_status(401)
+      end
+    end
+
+  end
 end
