62495096d3242fc6b9721cb371760be5b5e402c0
Michail Markov <avis-hermetis@yandex.ru>
2017-04-13 13:30:12 +1000

    Aditional fixes

--- app/models/concerns/votelogic.rb
+++ app/models/concerns/votelogic.rb
@@ -5,16 +5,16 @@ module Votelogic
 
     def vote(direction, user)
       transaction do
-        if vote = set_vote_by(user)
-          reset_vote(direction, vote)
+        if vote = find_vote_by(user)
+          change_vote(direction, vote)
         else
-          set_vote(direction)
+          create_vote(direction)
         end
       end
 
     end
 
-    def set_vote(direction)
+    def create_vote(direction)
       if direction == :up
         increment!(:rating, by = 1)
         votes.create(user_id: user.id, value: 1) && 0
@@ -24,7 +24,7 @@ module Votelogic
       end
     end
 
-    def reset_vote(direction, vote)
+    def change_vote(direction, vote)
       if direction == :up && vote.value == -1
         increment!(:rating, by = 1)
         vote.destroy && vote.value
@@ -34,12 +34,12 @@ module Votelogic
       end
     end
 
-    def set_vote_by(user)
+    def find_vote_by(user)
       votes.find_by(user_id: user.id)
     end
 
     def vote_value(user)
-      vote = set_vote_by(user) if user.present?
+      vote = find_vote_by(user) if user.present?
       vote.value if vote.present?
     end
 
